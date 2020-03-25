d94a14d0cdaef29d36dee6add935572329e68290
Kit Tejido <tejidokit@gmail.com>
2020-03-25 15:38:09 -0600

    js

---
+++ sandbox.js
@@ -0,0 +1,81 @@
+/*
+//################### STRINGS ####################
+console.log('hello world');
+
+let email = 'tejidokit@gmail.com';
+console.log(email);
+
+//string concatenation
+
+let firstName =  'Kit';
+let lastName =  'Tejido';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[0]);                 
+//string length                                         
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLocaleLowerCase();
+console.log(result);
+console.log(result, fullName);
+//##############
+let index = email.indexOf('@')
+console.log(index);
+//##############
+*/
+/*
+// common string methods
+let email = 'tejidokit@gmail.com';
+let result = email.lastIndexOf('e'); //answer on log would be 0
+console.log(result);
+*/
+/*
+// common string methods
+let email = 'tejidokit@gmail.com';
+let result = email.slice(0,3); //splices the first 3 letters and shows them so ans on log would be -> tej
+console.log(result);
+*/
+//##############
+/*
+let email = 'tejidokit@gmail.com';
+let result = email.substring(4,10); //grabs fromt he fourth character until it reached 10 characters
+console.log(result);
+*/
+/*
+//##############
+let email = 'tejidokit@gmail.com';
+//let result = email.replace('t', 'o'); //changes 't' to 'o'.
+let result = email.replace('i', 'g'); // only replaces the first 'i' to 'g'
+console.log(result);
+*/
+
+
+//##################### NUMBERS #####################
+let radius = 10;
+const pi = 3.14;
+//console.log(radius, pi); <-- just regular variable
+// let result = radius % 3; <-- just regular variable but divided by 3
+//let result = pi * radius**2 <-- pi x radius to the power of 2
+
+//################ ORDER OF OPERATION #####################
+// Brackets Exponents Division Multiplication Addition Subtraction
+//console.log(result);
+
+//################ ORDER OF OPERATION #####################
+/*
+let likes = 10;
+likes = likes + 1; // <-- 10 + 1 = 11
+console.log(likes);
+
+        or
+
+let likes = 10;
+likes++; 
+console.log(likes);
+
+*/
