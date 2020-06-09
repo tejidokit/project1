d8b4b974023c6cf4c46be8da382ef77e95fc9ddc
Kit Tejido <tejidokit@gmail.com>
2020-06-08 23:04:02 -0600

    reduce method

--- sandbox.js
+++ sandbox.js
@@ -1745,16 +1745,77 @@ console.log(result);
 
 // ######### FILTER METHOD ########
 
-const users = [
-    {name: 'yoshimitsu', premium: true},
-    {name: 'akira', premium: false},
-    {name: 'yusuke', premium: false},
-    {name: 'sakura', premium: true},
-];
+// const users = [
+//     {name: 'yoshimitsu', premium: true},
+//     {name: 'akira', premium: false},
+//     {name: 'yusuke', premium: false},
+//     {name: 'sakura', premium: true},
+// ];
 
-const premiumUsers = users.filter((user) => {
-    return user.premium;
-});
+// const premiumUsers = users.filter((user) => {
+//     return user.premium;
+// });
 
-console.log(premiumUsers);
-//ans:
+// console.log(premiumUsers);
+// //ans: 0: Object { name: "yoshimitsu", premium: true }
+// 1: Object { name: "sakura", premium: true }
+// length: 2
+
+
+
+// ############ MAP METHOD ###############
+
+// const prices = [20, 10, 30, 25, 15, 40, 85, 5];
+
+// const salePrices = prices.map((price) => {
+//     return price / 2;
+// });
+
+// console.log(sale.Prices);
+
+
+
+// ### change products of price over 30 without editing the original variable/value containing the array
+
+// const products = [
+//     {name: 'gold star', price: 20},
+//     {name: 'mushroom', price: 40},
+//     {name: 'green shells', price: 30},
+//     {name: 'banana skin', price: 10},
+//     {name: 'red shells', price: 50},
+// ];
+
+// const saleProducts = products.map((product) => {
+//     if(product.price > 30){
+//         return {name: product.name, price: product.price / 2}
+//     } else {
+//         return product;
+//     }
+// });
+
+// console.log(saleProducts);
+
+//ans below: 
+// 0: Object { name: "gold star", price: 20 }
+// ​
+// 1: Object { name: "mushroom", price: 20 }
+// ​
+// 2: Object { name: "green shells", price: 30 }
+// ​
+// 3: Object { name: "banana skin", price: 10 }
+// ​
+// 4: Object { name: "red shells", price: 25 }
+
+
+// ############ REDUCE METHOD ###############
+
+const scores = [10, 20, 60, 40, 70, 90, 30];
+
+const result = scores.reduce((acc, curr) => {
+    if (curr > 50){
+        acc++;
+    }
+    return acc;
+}, 0);
+
+console.log(result);
